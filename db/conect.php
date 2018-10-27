583052086f12169c42290c9fc99a04be673c18e4
mozn <mozn.lozn2000@gmail.com>
2018-10-27 21:22:18 +0200

    playing with video/pacture taking from the webcam

--- db/conect.php
+++ db/conect.php
@@ -39,9 +39,9 @@ try {
 	lastname VARCHAR(30) NOT NULL,
 	email VARCHAR(50),
 	pssword VARCHAR(255) NOT NULL , 
-	verified TINYINT(1) NOT NULL
+	confirm VARCHAR(255) NOT NULL , 
+	active TINYINT(1) NOT NULL
 	)";
-
 	// use exec() because no results are returned
 	$conn->exec($sql);
 	echo "Table ".$usrsTB." created successfully";
@@ -54,39 +54,39 @@ catch(PDOException $e)
 /************************************/
 /*			Insert Data 	   		*/
 /************************************/
-try {
-	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-	// set the PDO error mode to exception
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// try {
+// 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+// 	// set the PDO error mode to exception
+// 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	// prepare sql and bind parameters
-	$stmt = $conn->prepare("INSERT INTO ".$usrsTB." 
-	(username, firstname, lastname, email, pssword, verified) 
-	VALUES (:username, :firstname, :lastname, :email, :pssword, :verified)");
+// 	// prepare sql and bind parameters
+// 	$stmt = $conn->prepare("INSERT INTO ".$usrsTB." 
+// 	(username, firstname, lastname, email, pssword, verified) 
+// 	VALUES (:username, :firstname, :lastname, :email, :pssword, :verified)");
 
 
-	$stmt->bindParam(':username', $username);
-	$stmt->bindParam(':firstname', $firstname);
-	$stmt->bindParam(':lastname', $lastname);
-	$stmt->bindParam(':email', $email);
-	$stmt->bindParam(':pssword', $pssword);
-	$stmt->bindParam(':verified', $verified);
+// 	$stmt->bindParam(':username', $username);
+// 	$stmt->bindParam(':firstname', $firstname);
+// 	$stmt->bindParam(':lastname', $lastname);
+// 	$stmt->bindParam(':email', $email);
+// 	$stmt->bindParam(':pssword', $pssword);
+// 	$stmt->bindParam(':verified', $verified);
 
-	// insert a row
-	$username = $_POST('username');
-	$firstname = $_POST('firstname');
-	$lastname = $_POST('lastname');
-	$email = $_POST('email');
-	$pssword = $_POST('pssword');
-	$verified = $_POST('verified');
-	$stmt->execute();
+// 	// insert a row
+// 	$username = $_POST('username');
+// 	$firstname = $_POST('firstname');
+// 	$lastname = $_POST('lastname');
+// 	$email = $_POST('email');
+// 	$pssword = $_POST('pssword');
+// 	$verified = $_POST('verified');
+// 	$stmt->execute();
 
-	echo "New records created successfully";
-}
-catch(PDOException $e)
-{
-	echo "Error: " . $e->getMessage();
-}
+// 	echo "New records created successfully";
+// }
+// catch(PDOException $e)
+// {
+// 	echo "Error: " . $e->getMessage();
+// }
 
 
 $conn = null;
--- index.php
+++ index.php
@@ -27,8 +27,14 @@ if ($sess) {
 </section>
 
 <section class="shadow-lg p-3 mb-5 bg-white " id="main">
-	<h2>main</h2>
+	<h2>MaiN</h2>
 	<!-- echo ("<div class=".$type.">".$message."</div>"); -->
+	<div id="myOnlineCamera">
+		<video></video>
+		<canvas></canvas>
+		<button>Take Photo!</button>
+	</div>
+	
 	<?php
 	echo ("<div class=".$type.">".$message."</div>");
 	// $_SESSION['type'] = "";
@@ -36,11 +42,71 @@ if ($sess) {
 	?>
 	<div>
 		SELECT * FROM TABLE
+		<!-- <center>
+			<video id="video" width="640" height="480" autoplay></video>
+		</center> -->
 	</div>
 </section>
 
 <section class="shadow-lg p-3 mb-5 bg-white " id="footer">
 	<h2>footer</h2>
 </section>
+<script>
+// var video = document.getElementById('video');
+// if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//  navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+//  video.src = window.URL.createObjectURL(stream);
+//  video.play();
+//  });
+// }
+
+
+function startWebcam(stream){
+
+var myOnlineCamera    = getElementById('myOnlineCamera'),
+	video            = myOnlineCamera.querySelectorAll('video'),
+	canvas            = myOnlineCamera.querySelectorAll('canvas');
+
+video.width = video.offsetWidth;
+
+if(navigator.getUserMedia){                    // Standard
+	video.src = stream;
+	video.play();
+}else if(navigator.webkitGetUserMedia){        // WebKit
+	video.src = window.webkitURL.createObjectURL(stream);
+	video.play();
+}else if(navigator.mozGetUserMedia){        // Firefox
+	video.src = window.URL.createObjectURL(stream);
+	video.play();
+};
+
+// Click to take the photo
+$('#webcam-popup .takephoto').click(function(){
+	// Copying the image in a temporary canvas
+	var temp = document.createElement('canvas');
+
+	temp.width  = video.offsetWidth;
+	temp.height = video.offsetHeight;
+
+	var tempcontext = temp.getContext("2d"),
+		tempScale = (temp.height/temp.width);
+
+	temp.drawImage(
+		video,
+		0, 0,
+		video.offsetWidth, video.offsetHeight
+	);
+
+	// Resize it to the size of our canvas
+	canvas.style.height    = parseInt( canvas.offsetWidth * tempScale );
+	canvas.width        = canvas.offsetWidth;
+	canvas.height        = canvas.offsetHeight;
+	var context        = canvas.getContext("2d"),
+		scale        = canvas.width/temp.width;
+	context.scale(scale, scale);
+	context.drawImage(bigimage, 0, 0);
+});
+};
+</script>
 
 <?php include 'frame/tail.php'; ?>
